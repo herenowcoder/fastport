54cc1f51133f8a43bfe394b129b1dbe848be32dd
Wojciech Kaczmarek <wk@dualtech.com.pl>
2010-11-24 12:34:38 +0100

    bsearch.rb: more natural flow in a loop

--- bsearch.rb
+++ bsearch.rb
@@ -3,7 +3,23 @@
 def bsearch f, &compare
   prev_range = []
   loop = lambda do |p1, p2|
-    if [p1,p2] == prev_range
+    if [p1,p2] != prev_range
+      prev_range = [p1,p2]
+      f.seek(p1 + ((p2-p1).div 2))
+      f.gets # scratch this line remainder
+      p = f.pos
+      line = f.gets
+      return loop.call *prev_range if line.nil?
+      line.chomp!
+      r = compare.call line
+      if r < 0
+        loop.call p1, p
+      elsif r > 0
+        loop.call f.pos, p2
+      else
+        return line
+      end
+    else # final search
       f.seek p1
       while (line = f.gets) do
         line.chomp!
@@ -11,21 +27,6 @@ def bsearch f, &compare
         return line if r == 0 
       end
       return :not_found
-    end
-    prev_range = [p1,p2]
-    f.seek(p1 + ((p2-p1).div 2))
-    f.gets # scratch this line remainder
-    p = f.pos
-    line = f.gets
-    return loop.call *prev_range if line.nil? 
-    line.chomp!
-    r = compare.call line
-    if r < 0
-      loop.call p1, p
-    elsif r > 0
-      loop.call f.pos, p2
-    else
-      return line
     end
   end
   loop.call 0, f.size 
